90d9b19ee06779455da0e165268552ac8e5391f5
Klishin Ilya <klishin-ilya@mail.ru>
2021-01-15 22:24:30 +0300

    Initial commit

---
+++ src/test/java/ru/vsu/pet/tester/TesterApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.vsu.pet.tester;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TesterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
